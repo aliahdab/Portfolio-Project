c4f9c84adf786e7227a653aac94ae10184d5bb9e
Ali Ahdab <aliahdab@gmail.com>
2019-10-31 23:48:28 +0100

    adding about us

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
       <div >
         <section className="about-continuer-section">
           <div className="box_1620">
-            <div class="abour-container-div">
+            <div class="about-container-div">
               <div class="banner_content text-center">
                 <h2>About Us</h2>
                 <div className="page_link">
