892030b7c24324a9ba392be1ba2b073009d456f3
Ali Ahdab <aliahdab@gmail.com>
2019-10-31 23:47:05 +0100

    adding about us

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class About extends Component {
 
-    render() {
-        return (
-            <div >
-
+  render() {
+    return (
+      <div >
+        <section className="about-continuer-section">
+          <div className="box_1620">
+            <div class="abour-container-div">
+              <div class="banner_content text-center">
+                <h2>About Us</h2>
+                <div className="page_link">
+                  <a href="home.html">Home</a>
+                  <a href="about.html">About Us</a>
+                </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </section>
+      </div>
+    )
+  }
 }
 export default About;
