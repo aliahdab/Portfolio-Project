6a081c41f7cb208179197b56120c5f1ea4d30579
Eid <mohamadeid.da@gmail.com>
2019-11-03 17:43:33 +0100

    adding pages address in navigation bar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import Experience from './components/Experience'
 class App extends Component {
   
   scrollToHome = () => {
+    window.history.pushState(null, null, 'home')
     window.scrollTo(0, 0);
   }
   scrollToExperience = () => {
+    window.history.pushState(null, null, 'experience')
     window.scrollTo(0, 720);
   }
   scrollToSkills = () => {
+    window.history.pushState(null, null, 'skills')
     window.scrollTo(0, 1400);
   }
   scrollToProjects = () => {
+    window.history.pushState(null, null, 'projects')
     window.scrollTo(0, 2100);
   }
 
