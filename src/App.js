4e3197fc079ead9ec646aa056a1e942a32098dfc
Ali Ahdab <aliahdab@gmail.com>
2019-11-03 22:36:37 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/Home'
 import Navigation from './components/Navigation'
 import Experience from './components/Experience'
+import Admin from './components/Admin'
 
 class App extends Component {
-  
+
   scrollToHome = () => {
     window.history.pushState(null, null, 'home')
     window.scrollTo(0, 0);
@@ -21,16 +23,21 @@ class App extends Component {
   scrollToProjects = () => {
     window.history.pushState(null, null, 'projects')
     window.scrollTo(0, 2100);
+
   }
+
+
 
   render() {
     return (
-
-      <div className="App">
-        <Navigation scrollToHome = {this.scrollToHome} scrollToExperience = {this.scrollToExperience} scrollToSkills = {this.scrollToSkills} scrollToProjects = {this.scrollToProjects}></Navigation>
-        <Home></Home>
-        <Experience ></Experience>
-      </div>
+      <Router>
+        <div className="App">
+          <Navigation scrollToHome={this.scrollToHome} scrollToExperience={this.scrollToExperience} scrollToSkills={this.scrollToSkills} scrollToProjects={this.scrollToProjects}></Navigation>
+          <Home></Home>
+          <Experience ></Experience>
+          <Admin></Admin>
+        </div>
+      </Router>
     )
 
   }
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class Admin extends Component {
+export class Admin extends Component {
+  render() {
+    return (
+      <section className="admin-container">
+        <form>
+          <label className="admin-project-name">
+            Project Name:
+             <input type="text" name="name" />
+          </label>
+          <label className="admin-project-des">
+            Project Description:
+             <input type="text" name="description" />
+          </label>
+          <label className="admin-project-link">
+            Project Link on Git:
+             <input type="text" name="link" />
+          </label>
+          <label className="admin-project-create-date">
+            Project Creation Date:
+             <input type="text" name="link" />
+          </label>
 
-    render() {
-        return (
-            <div >
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+    )
 
-            </div>
-        )
-    }
+  }
 }
-export default Admin;
+
+export default Admin
