ffa0e326c6b3008a63d85e782b4cf65984860884
Ali Ahdab <aliahdab@gmail.com>
2019-11-04 08:43:43 +0100

    add target="_blank"

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Experience from './components/Experience';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Skills from './components/Skills';
 import Projects from './components/Projects.js';
+import Admin from './components/Admin.js';
 
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/experience" component={Experience} />
             <Route path="/skills" component={Skills} />
             <Route path="/projects" component={Projects} />
+            <Route path="/About" component={Admin} />
             <Route path="/" exact component={Home} />
           </Switch>
         </div>
