a091d2e879640609cd6b3421e45cc217724b9337
Ali Ahdab <aliahdab@gmail.com>
2019-11-05 01:55:32 +0100

    add admin page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -5,30 +5,28 @@ export class Admin extends Component {
     return (
       <section className="admin-container">
         <form className="admin-form">
-          <h2> Add a project
-          </h2>
+          <h2> Add a project</h2>
           <label className="admin-label">
             Project Name:
-             <input type="text" name="name" />
+             <input type="text" name="name" required />
+          </label>
+          <address className="admin-label">
+            Project Address:
+             <input type="text" name="link" required />
+          </address>
+          <label className="admin-label">
+            Project Creation Date:
+             <input type="text" name="link" required />
           </label>
           <label className="admin-label">
             Project Description:
              <textarea type="text" name="description" />
-          </label>
-          <address className="admin-label">
-            Project Address:
-             <input type="text" name="link" />
-          </address>
-          <label className="admin-label">
-            Project Creation Date:
-             <input type="text" name="link" />
           </label>
 
           <input type="submit" value="Submit" />
         </form>
       </section>
     )
-
   }
 }
 
