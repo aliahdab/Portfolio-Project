7a527c0fd74833aba598fe1a91a159d9ca6d78e2
Ali Ahdab <aliahdab@gmail.com>
2020-04-05 11:04:03 +0200

    Updates

---
+++ precache-manifest.e8e33d132108499ccd18233010b094fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8849fc5ceb9baa3b1903dccd7deb844",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "260219f2fb934204c16d",
+    "url": "/portfolio/static/css/main.142fd23c.chunk.css"
+  },
+  {
+    "revision": "0c4cc40dc16bb7fdbd04",
+    "url": "/portfolio/static/js/2.7e099ef5.chunk.js"
+  },
+  {
+    "revision": "260219f2fb934204c16d",
+    "url": "/portfolio/static/js/main.49295bbe.chunk.js"
+  },
+  {
+    "revision": "6c1a3b268f33b11a7a71",
+    "url": "/portfolio/static/js/runtime-main.ecd6df18.js"
+  },
+  {
+    "revision": "43983bc18096af436a856d5d24c82692",
+    "url": "/portfolio/static/media/Megan_Creegan.43983bc1.jpg"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/portfolio/static/media/background.d078968b.jpg"
+  }
+]);
