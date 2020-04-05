018afb3aa70825f606d6ed682afa11dd8494f143
Ali Ahdab <aliahdab@gmail.com>
2020-04-05 11:07:31 +0200

    Updates

---
+++ precache-manifest.3d6dc5cb466c415d8acde68c33123450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a06e68d7cf2897cabb840505e043771",
+    "url": "/Portfolio-Project/index.html"
+  },
+  {
+    "revision": "ad637744a1e9dcb5a78a",
+    "url": "/Portfolio-Project/static/css/main.99795284.chunk.css"
+  },
+  {
+    "revision": "a3853aa3e150ba9d6ed4",
+    "url": "/Portfolio-Project/static/js/2.15e0c850.chunk.js"
+  },
+  {
+    "revision": "ad637744a1e9dcb5a78a",
+    "url": "/Portfolio-Project/static/js/main.2e32c225.chunk.js"
+  },
+  {
+    "revision": "1ae1a6989d23e15eb627",
+    "url": "/Portfolio-Project/static/js/runtime-main.bd3618c5.js"
+  },
+  {
+    "revision": "43983bc18096af436a856d5d24c82692",
+    "url": "/Portfolio-Project/static/media/Megan_Creegan.43983bc1.jpg"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/Portfolio-Project/static/media/background.d078968b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e8e33d132108499ccd18233010b094fd.js"
+  "/Portfolio-Project/precache-manifest.3d6dc5cb466c415d8acde68c33123450.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-Project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
